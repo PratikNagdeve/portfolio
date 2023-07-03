1ae083b29dded601ad82c7a4c1584b25fd01d63e
pratiknagdeve <pratiknagdeve7@gmail.com>
2023-07-03 19:12:25 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
-  
-  downloadCV()
-  {
-  const documentUrl = 'document_url';
-  const fileName = this.getFileNameFromUrl(documentUrl);
-
-  const link = document.createElement('a');
-  link.href = documentUrl;
-  link.download = fileName;
-  link.target = '_blank';
-  link.style.display = 'none';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  }
-
-  getFileNameFromUrl(url: string): string {
-    const index = url.lastIndexOf('/');
-    return url.substring(index + 1);
-  }
 
 }
